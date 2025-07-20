d8584f3f5a3f8c16a88ad14f7a3fa028e93e901a
prog4321 <dca4321@yahoo.com>
2025-07-20 23:17:59 +0800

    change port number

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import pg from "pg";
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = 10000;
 
 var db = new pg.Client({
   user: "alphamert_user",
